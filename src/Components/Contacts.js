0a54db08c907f42368c684a76dd5e86b31491781
Young Professor <113968776+Young-Professor@users.noreply.github.com>
2023-03-03 01:00:22 +0300

    Contacts

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -9,7 +9,7 @@ const Contacts = () => {
       </h1>
       <div className='text-white'>
         <p>
-          <span className='text-yellow-500'>Phone Number:</span> 0729144533
+          <span className='text-yellow-500'>Phone Number:</span> +254729144533
         </p>
         <p>
           <span className='text-yellow-500'>Email:</span> jgathiru02@gmail.com
@@ -27,59 +27,7 @@ const Contacts = () => {
   style={{ minWidth: "100%", maxWidth: "100%", height: "80vh", border: "none", backgroundColor: "transparent" }}
   scrolling="no"
 ></iframe> */}
-  <div className="footer-contact">
-  <form action="https://formspree.io/your-email-address" method="POST">
-    <h3>Contact Us</h3>
-    <div className="form-group">
-      <label htmlFor="message">Message:</label>
-      <textarea
-        id="message"
-        name="message"
-        rows="3"
-        required
-      ></textarea>
-    </div>
-    <div className="form-flex">
-      <div className="form-group">
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          required
-        />
-      </div>
-    </div>
-    <div className="form-group">
-      <label htmlFor="_replyto">Your Email:</label>
-      <input
-        type="email"
-        id="_replyto"
-        name="_replyto"
-        required
-      />
-    </div>
-    <div className="form-group">
-      <label htmlFor="_subject">Subject:</label>
-      <input
-        type="text"
-        id="_subject"
-        name="_subject"
-        required
-      />
-    </div>
-    <button type="submit">Send</button>
-  </form>
-</div>
+  
 
 
 
