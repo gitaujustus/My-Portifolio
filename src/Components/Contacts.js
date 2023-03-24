fa4fc9ef4779bbbcfcf5258efc7dd0c98dd479d3
Young Professor <113968776+Young-Professor@users.noreply.github.com>
2023-03-24 17:30:39 +0300

    Added: source

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -28,7 +28,7 @@ const Contacts = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     isLoading(true);
-    const data = { name, email, message, source:'Message from Portifolio' };
+    const data = { name, email, message, source:'Message from Portifolio website' };
     console.log(data);
     //https://nodemailer-server-rouge.vercel.app/
     try {
